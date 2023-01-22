0b2902adf893c72680e22f30743828ada76df010
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-01-22 20:48:57 +0100

    feat: Added pigeon code generation scripts
    The code is generated in the **/generated/** folders

---
+++ run_pigeon.ps1
@@ -0,0 +1,8 @@
+flutter pub run pigeon `
+    --input pigeons/api_sensor_manager.dart `
+    --dart_out lib/src/generated/api_sensor_manager.dart `
+    --objc_header_out ios/Classes/Generated/ApiSensorManager.h `
+    --objc_source_out ios/Classes/Generated/ApiSensorManager.m `
+    --objc_prefix FLT `
+    --java_out android/src/main/kotlin/de/uniulm/sensing_plugin/generated/ApiSensorManager.java `
+    --java_package "de.uniulm.sensing_plugin.generated"
