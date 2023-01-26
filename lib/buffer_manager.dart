6ad4ecb08504172fcc717f25d4e35c76e4eeb396
Leonhard Alkewitz <leonhard@alkewitz.net>
2023-01-26 17:16:22 +0100

    fix: syntax errors for reading from database (solution: cast result from database request instantly to sensor event and provide sensor event as object, which can be later 'casted' to string)

--- lib/buffer_manager.dart
+++ lib/buffer_manager.dart
@@ -7,97 +7,95 @@ import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
 import 'package:sembast/timestamp.dart';
 import 'package:sensor_demo/sensor_data.dart';
+import 'package:sensor_demo/sensor_event.dart';
 
+class BufferManager {
+  BufferManager._privateConstructor() {
+    openDB();
+  }
+  static final BufferManager _instance = BufferManager._privateConstructor();
 
-class BufferManager{
+  final HashMap _buffers = HashMap<int, List<SensorData>>();
+  late final Database db;
+  late final StoreRef store;
+  bool init = false;
 
-BufferManager._privateConstructor(){
-  openDB();
-}
-static final BufferManager _instance = BufferManager._privateConstructor();
-
-final HashMap _buffers = HashMap<int, List<SensorData>>();
-late final  Database db;
-late final StoreRef store;
-bool init = false;
-
-factory BufferManager() {
+  factory BufferManager() {
     return _instance;
   }
 
-void addBuffer(int id) {
- if(!_buffers.containsKey(id)){
-  _buffers[id] = <SensorData>[];
- }
-} 
+  void addBuffer(int id) {
+    if (!_buffers.containsKey(id)) {
+      _buffers[id] = <SensorData>[];
+    }
+  }
 
-void openDB() async {
+  void openDB() async {
 // get the application documents directory
-var dir = await getApplicationDocumentsDirectory();
+    var dir = await getApplicationDocumentsDirectory();
 // make sure it exists
-await dir.create(recursive: true);
+    await dir.create(recursive: true);
 // build the database path
-var dbPath = join(dir.path, 'my_database.db');
+    var dbPath = join(dir.path, 'my_database.db');
 // open the database
-db = await databaseFactoryIo.openDatabase(dbPath);
-store = intMapStoreFactory.store('product');
-init = true;
-}
-
-void fillBuffer(int id, SensorData data){
-  if(_buffers.containsKey(id)){
-  List<SensorData> list =  _buffers[id];
-  list.add(data);
-  writeToDatabase(data);
-  print(data.time.toIso8601String());
- }
-}
-SensorData latestData(int id){
-if(_buffers.containsKey(id)){
-  List<SensorData> list =  _buffers[id];
-  return list.last;
- }
- return SensorData(0, 0, 0);
-}
-
-SensorData medianAllData(int id){
-  if(_buffers.containsKey(id)){
-  List<SensorData> tmpSensorList = <SensorData>[];
-  List<SensorData> list =  _buffers[id];
-  tmpSensorList.addAll(list);
-
-  double medx = 0;
-  double medy = 0;
-  double medz = 0;
-  for (var element in tmpSensorList) {
-    medx += element.x;
-    medy += element.y;
-    medz += element.z;
-  } 
-  int max = tmpSensorList.length;
-  return SensorData(medx/max,medy/max,medz/max);
-  }
-  return SensorData(0,0,0);
-}
-
-void writeToDatabase(SensorData data) async{
-  store.add(db,{"x": data.x,"y": data.y, "z": data.z, "time":data.time});
-}
-
-Future<List<RecordSnapshot>> readFromDatabase() async{
-  var finder = Finder(
-  filter: Filter.lessThan('time', Timestamp.now()),
-  sortOrders: [SortOrder('time')]);
-  var records = await store.find(db, finder: finder);
-  return records;
+    db = await databaseFactoryIo.openDatabase(dbPath);
+    store = intMapStoreFactory.store('product');
+    init = true;
   }
 
-  
-  bool isDatabaseReady(){
-  return init;
+  void fillBuffer(int id, SensorData data) {
+    if (_buffers.containsKey(id)) {
+      List<SensorData> list = _buffers[id];
+      list.add(data);
+      writeToDatabase(data);
+      print(data.time.toIso8601String());
+    }
+  }
+
+  SensorData latestData(int id) {
+    if (_buffers.containsKey(id)) {
+      List<SensorData> list = _buffers[id];
+      return list.last;
+    }
+    return SensorData(0, 0, 0);
+  }
+
+  SensorData medianAllData(int id) {
+    if (_buffers.containsKey(id)) {
+      List<SensorData> tmpSensorList = <SensorData>[];
+      List<SensorData> list = _buffers[id];
+      tmpSensorList.addAll(list);
+
+      double medx = 0;
+      double medy = 0;
+      double medz = 0;
+      for (var element in tmpSensorList) {
+        medx += element.x;
+        medy += element.y;
+        medz += element.z;
+      }
+      int max = tmpSensorList.length;
+      return SensorData(medx / max, medy / max, medz / max);
+    }
+    return SensorData(0, 0, 0);
+  }
+
+  void writeToDatabase(SensorData data) async {
+    store.add(db, {"x": data.x, "y": data.y, "z": data.z, "time": data.time});
+  }
+
+  Future<List<SensorEvent>> readFromDatabase() async {
+    var finder = Finder(
+        filter: Filter.lessThan('time', Timestamp.now()),
+        sortOrders: [SortOrder('time')]);
+    var records = await store.find(db, finder: finder);
+    return records
+        .map((record) =>
+            SensorEvent.fromMap(record.value as Map<dynamic, dynamic>))
+        .toList(growable: false);
+  }
+
+  bool isDatabaseReady() {
+    return init;
+  }
 }
-
-}
-  
-
-
