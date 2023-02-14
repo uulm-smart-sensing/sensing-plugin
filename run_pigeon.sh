2507f472c936d4942043b40859c2bf4b8bac6f94
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-02-14 14:44:05 +0100

    refactor: refactored pigeon script

--- run_pigeon.sh
+++ run_pigeon.sh
@@ -2,8 +2,6 @@ echo "Generating Pigeon code..."
 flutter pub run pigeon \
     --input pigeons/api_sensor_manager.dart \
     --dart_out lib/src/generated/api_sensor_manager.dart \
-    --objc_header_out ios/Classes/Generated/ApiSensorManager.h \
-    --objc_source_out ios/Classes/Generated/ApiSensorManager.m \
-    --objc_prefix FLT \
+    --experimental_swift_out ios/Classes/Generated/ApiSensorManager.swift \
     --java_out android/src/main/kotlin/de/uniulm/sensing_plugin/generated/ApiSensorManager.java \
     --java_package "de.uniulm.sensing_plugin.generated"
