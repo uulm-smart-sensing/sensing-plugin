df8c7aa4480220439f242e223cccc85b778eaa0b
Leonhard Alkewitz <leonhard@alkewitz.net>
2023-03-11 16:27:01 +0100

    refactor: added a collection of the 'sensor manager' existing in swift, so not multiple instances of this managers are created

---
+++ ios/Classes/CMManagerCollection.swift
@@ -0,0 +1,32 @@
+//
+//  CMManagerCollection.swift
+//  sensing_plugin
+//
+//  Created by Leonhard Alkewitz on 11.03.23.
+//
+
+import Foundation
+import CoreMotion
+
+/**
+ This object is a collection of all needed "sensor manager" existing in Swift, which can be used to access sensors
+ 
+ Therefor this class provide instances for the different manager (simalar to a collection singleton instances), so the
+ different sensor stream handler can access them.
+ - Important: This collection is used to prevent, that the manager (e. g. the CMMotionManager) are created
+              multiple times, because this is not recommended (c. f. official Swift documentation for
+              CMMotionManager)
+ */
+public class CMMangerCollection : NSObject {
+    
+    private static var motionManager : CMMotionManager?
+    
+
+    static func getMotionManager() -> CMMotionManager {
+        if (self.motionManager == nil) {
+            self.motionManager = CMMotionManager()
+        }
+        
+        return self.motionManager!
+    }
+}
--- ios/Classes/Sensors/GyroscopeHandler.swift
+++ ios/Classes/Sensors/GyroscopeHandler.swift
@@ -8,34 +8,43 @@
 import Foundation
 import CoreMotion
 
+/**
+ The object for starting and stopping the gyroscope sensor and receiving its sensor data
+ 
+ The ``GyroscopeHandler``wraps the part of the ``CMMotionManager``, which handles the
+ gyroscope sensor.
+ 
+ So this handler provide methods to ...
+ - check, whether the gyroscope is available or already used
+ - change the time interval at which the gyroscope data are collected
+ - "start" and "stop" the gyroscope, so whether the gyroscope should provide rotation data or not
+ 
+ - Important: Therefor it conforms the ``ISensorStreamHandler`` protocol, so it
+ can be called and managed by the ``SensorManager``.
+ 
+ */
 public class GyroscopeHandler : NSObject, ISensorStreamHandler {
     
     static var sensorId: SensorIds = SensorIds.gyroscope
-    
-    private var motionManager: CMMotionManager
-    
-    internal override init() {
-        self.motionManager = CMMotionManager()
-    }
-    
+        
     func isSensorAvailable() -> Bool {
-        return self.motionManager.isGyroAvailable
+        return CMMangerCollection.getMotionManager().isGyroAvailable
     }
     
     func isSensorUsed() -> Bool {
-        return self.motionManager.isGyroActive
+        return CMMangerCollection.getMotionManager().isGyroActive
     }
     
     /**
      - Warning: The supported time interval depends on device, but it can assumed that it is at least 100Hz. This means,
-                time intervals, which are bigger than 10ms should work without any problems.
-                But if the time interval is smaller than 10ms it is possible,
-                that the update will not be performed or the change can not be notified.
+     time intervals, which are bigger than 10ms should work without any problems.
+     But if the time interval is smaller than 10ms it is possible,
+     that the update will not be performed or the change can not be notified.
      */
     func changeSensorTimeInterval(timeInterval: Int32) -> ResultWrapper {
         // convert time interval from miliseconds into seconds
         let timeIntervalInSec : TimeInterval = Double(timeInterval) / 1000
-        self.motionManager.gyroUpdateInterval = timeIntervalInSec;
+        CMMangerCollection.getMotionManager().gyroUpdateInterval = timeIntervalInSec;
         
         // check, whether the requested time interval is below 10 ms
         if (timeInterval < 10) {
@@ -50,14 +59,14 @@ public class GyroscopeHandler : NSObject, ISensorStreamHandler {
     // TODO: check the accuracy of the gyroscope and do not return -1 (indicating no information) as accuracy
     func getSensorInfo() -> SensorInfo {
         // convert time interval from seconds to milliseconds
-        let timeIntervalInMilliSec : Int32 = Int32(self.motionManager.gyroUpdateInterval * 1000)
+        let timeIntervalInMilliSec : Int32 = Int32(CMMangerCollection.getMotionManager().gyroUpdateInterval * 1000)
         return SensorInfo(unit: Unit.radiansPerSecond, accuracy: -1, timeIntervalInMilliseconds: timeIntervalInMilliSec)
     }
     
     // TODO: check, whether timer is not the better solution and provide the data at more precise frequency or if then data would be losen
     public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         if (isSensorAvailable()) {
-            motionManager.startGyroUpdates(to: OperationQueue.current!, withHandler: {(gyroscopeData: CMGyroData?, error: Error?) in
+            CMMangerCollection.getMotionManager().startGyroUpdates(to: OperationQueue.current!, withHandler: {(gyroscopeData: CMGyroData?, error: Error?) in
                 guard (error != nil) else {
                     // get sensor values from gyroscope
                     var xValue = gyroscopeData?.rotationRate.x
@@ -79,7 +88,7 @@ public class GyroscopeHandler : NSObject, ISensorStreamHandler {
     
     public func onCancel(withArguments arguments: Any?) -> FlutterError? {
         if (isSensorUsed()) {
-            self.motionManager.stopGyroUpdates()
+            CMMangerCollection.getMotionManager().stopGyroUpdates()
             return nil
         }
         return FlutterError(code: "NO_UPDATE_STOP_POSSIBLE", message: "Gyroscope is not used, so stopping the updates is not possible.", details: "")
