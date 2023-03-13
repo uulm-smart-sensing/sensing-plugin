4a4be5fb3d5ed52eb40dec8459cbcdf53a1792a9
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-03-13 18:42:36 +0100

    docs: Added hint on why we use list representation to send SensorData object

--- android/src/main/kotlin/de/uniulm/sensing_plugin/SensorDataExtensions.kt
+++ android/src/main/kotlin/de/uniulm/sensing_plugin/SensorDataExtensions.kt
@@ -2,7 +2,13 @@ package de.uniulm.sensing_plugin
 
 import de.uniulm.sensing_plugin.generated.ApiSensorManager.SensorData
 
-/** Converts this [SensorData] object to a list, with the class properties as list elements. */
+/**
+ * Converts this [SensorData] object to a list, with the class properties as list elements.
+ *
+ * This is used to send the [SensorData] object via an EventChannel to the Flutter side.
+ * Sending the object directly causes an error, therefore it is sent as list representation, which
+ * is the only way an object can be send via an EventChannel.
+ */
 fun SensorData.toList(): ArrayList<Any> {
     val list = ArrayList<Any>()
     list.add(this.data)
