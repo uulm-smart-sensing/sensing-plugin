42caed8c333e65da825118d1cf9c51b1a5252f52
Leonhard Alkewitz <leonhard@alkewitz.net>
2023-03-13 13:25:11 +0100

    style: adapted ios code to swiftlint conventions (as far as possible)

--- ios/Classes/Sensors/ISensorStreamHandler.swift
+++ ios/Classes/Sensors/ISensorStreamHandler.swift
@@ -11,32 +11,33 @@ import Foundation
 /**
  Interface for a stream handler of a sensor
  */
-protocol ISensorStreamHandler : NSObject, FlutterStreamHandler {
-    
+protocol ISensorStreamHandler: NSObject, FlutterStreamHandler {
+
     /**
      Checks, whether the sensor is available and can provide sensor data
-     - Returns: true, if the sensor is currently available and would provide data, if the tracking would be started, otherwise false
+     - Returns: true, if the sensor is currently available and would provide data,
+                if the tracking would be started, otherwise false
      */
     func isSensorAvailable() -> Bool
-    
-    
+
     /**
      Checks, whether the sensor is currently used, which means it was started and not stopped yet
      - Returns: true, if the sensor is currently used (so being tracked) and provide sensor data, otherwise false
      */
     func isSensorUsed() -> Bool
-    
+
     /**
      Changes the interval at which the sensor provide data
      - Parameters:
         - timeInterval: required interval in ms
      - Returns: ResultWrapper if the time interval was changed sucessfully, otherwise StateIndicator.failure
      */
-    func changeSensorTimeInterval(timeInterval : Int32) -> ResultWrapper
-    
+    func changeSensorTimeInterval(timeInterval: Int32) -> ResultWrapper
+
     /**
      Get the information about this sensor
-     - Returns: SensorInfo object containing information about the unit, accuracy and the time interval at which the data from the sensor are provided
+     - Returns: SensorInfo object containing information about the unit, accuracy and
+                the time interval at which the data from the sensor are provided
      */
     func getSensorInfo() -> SensorInfo
 }
