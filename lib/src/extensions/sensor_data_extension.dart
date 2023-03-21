526590d3d3764a4efdc089a3a470db3fa9f74af9
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-03-21 17:07:21 +0100

    feat: Added copyWith extension for SensorData

---
+++ lib/src/extensions/sensor_data_extension.dart
@@ -0,0 +1,33 @@
+import '../generated/api_sensor_manager.dart';
+
+/// Extension methods for [SensorData].
+extension SensorDataExtension on SensorData {
+  /// Copies this [SensorData] object and replaces each object variable with the
+  /// according parameter if the parameter is not null.
+  ///
+  /// Example:
+  /// ```dart
+  /// var sensorData = SensorData(
+  ///   data: [1, 2, 3],
+  ///   maxPrecision: 2,
+  ///   unit: Unit.celsius,
+  /// );
+  /// var copy = sensorData.copyWith(data: [2, 3, 6], unit: Unit.fahrenheit);
+  /// // copy is:
+  /// // SensorData(
+  /// //   data: [2, 3, 6],
+  /// //   maxPrecision: 2,
+  /// //   unit: Unit.fahrenheit
+  /// // )
+  /// ```
+  SensorData copyWith({
+    List<double?>? data,
+    int? maxPrecision,
+    Unit? unit,
+  }) =>
+      SensorData(
+        data: data ?? this.data,
+        maxPrecision: maxPrecision ?? this.maxPrecision,
+        unit: unit ?? this.unit,
+      );
+}
