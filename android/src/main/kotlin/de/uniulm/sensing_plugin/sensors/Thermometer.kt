aa61a5783c3837c5615c6e7ee1a5309af09b31a8
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-03-23 22:11:13 +0100

    feat: Added Thermometer

---
+++ android/src/main/kotlin/de/uniulm/sensing_plugin/sensors/Thermometer.kt
@@ -0,0 +1,21 @@
+package de.uniulm.sensing_plugin.sensors
+
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import de.uniulm.sensing_plugin.generated.ApiSensorManager.Unit
+import de.uniulm.sensing_plugin.streamhandlers.SingleSensorStreamHandler
+
+class Thermometer(
+    sensorManager: SensorManager,
+    timeIntervalInMilliseconds: Long,
+    unit: Unit
+) : SingleSensorStreamHandler(
+    sensorManager,
+    sensorId,
+    timeIntervalInMilliseconds,
+    unit
+) {
+    companion object {
+        const val sensorId = Sensor.TYPE_AMBIENT_TEMPERATURE
+    }
+}
