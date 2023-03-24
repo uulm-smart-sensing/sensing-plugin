19b2192c0fb2ef9c9f0066b27db00917f6b7a3e7
Leonhard Alkewitz <leonhard@alkewitz.net>
2023-03-24 16:38:47 +0100

    refactor: marked heading sensor as deprecated and adapted 'yaw' = azimuth value of orientation sensor to match the description of Android, extended documentation in orientation sensor

--- ios/Classes/Sensors/OrientationSensorHandler.swift
+++ ios/Classes/Sensors/OrientationSensorHandler.swift
@@ -22,15 +22,18 @@ import CoreMotion
  - "start" and "stop" the "orientation" sensor, so whether the "orientation" sensor should
     provide orientation angles  data or not
  
- Orientation angles are called "roll", "pitch" and "yaw" and define the angle of rotation
- around the x,y and z axis of the device.
+ Orientation angles are called "roll", "pitch" and "azimuth" and define the angle of rotation
+ around the x,y and z axis of the device relative to the reference frame, which is set
+ as
+ - x axis points to the magnet north
+ - z axis is orthogonally to the ground plane.
  
  - Important: Therefor it conforms the ``ISensorStreamHandler`` protocol, so it
  can be called and managed by the ``SensorManager``.
  
  */
 public class OrientationSensorHandler: NSObject, ISensorStreamHandler {
-
+    
     func isSensorAvailable() -> Bool {
         return ManagerCollection.getMotionManager().isDeviceMotionAvailable
     }
@@ -59,7 +62,8 @@ public class OrientationSensorHandler: NSObject, ISensorStreamHandler {
     public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         
         if isSensorAvailable() {
-            ManagerCollection.getMotionManager().startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {
+            ManagerCollection.getMotionManager().startDeviceMotionUpdates(using: .xMagneticNorthZVertical,
+                                                                          to: OperationQueue.current!, withHandler: {
                 (deviceMotionData: CMDeviceMotion?, err: Error?) in
                 guard err != nil else {
                     // multiplying "roll" and "pitch" with negative one to get the same sign / orientation like described
@@ -67,14 +71,21 @@ public class OrientationSensorHandler: NSObject, ISensorStreamHandler {
                     // (see https://developer.android.com/guide/topics/sensors/sensors_position#sensors-pos-orient)
                     let roll = -1 * (deviceMotionData?.attitude.roll)!
                     let pitch = -1 * (deviceMotionData?.attitude.pitch)!
-                    let yaw = deviceMotionData?.attitude.yaw
+                    
+                    // Because the "yaw" value is relative to the reference frame "xMagneticNorth" and
+                    // the angle is from -180 to 180 relative to the north, but expected as
+                    // 0 to 360, the "yaw" value would need to be converted. To spare this conversion,
+                    // the "heading" attribute is used, which ranges from 0 to 360 degrees and is relative to
+                    // the magnetic north (indicated by angle = 0). Because it is provided in degrees, it
+                    // need to be converted to radians
+                    let azimuth = (deviceMotionData?.heading)! * .pi / 180
                     
                     let timestamp = TimestampConverter.convertSensorEventToUnixTimestamp(
                         sensorEventTimestamp: deviceMotionData!.timestamp)
 
                     // TODO: check, what maxPrecision is
                     // wrap the sensor values to `SensorData` object and "send" it to the event stream
-                    let sensorData = SensorData(data: [roll, pitch, yaw],
+                    let sensorData = SensorData(data: [roll, pitch, azimuth],
                                                 maxPrecision: -1,
                                                 unit: Unit.radians,
                                                 timestampInMicroseconds: timestamp)
@@ -102,5 +113,5 @@ public class OrientationSensorHandler: NSObject, ISensorStreamHandler {
                             message: "Orientation sensor is not used, so stopping the updates is not possible.",
                             details: "")
     }
-
+    
 }
