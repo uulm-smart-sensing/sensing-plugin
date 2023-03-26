7e3aa4f9a71f7149fed4f2b56b30c058977b6285
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-03-26 01:18:20 +0100

    feat: Implemented Sensor Info Page

--- example/lib/pages/sensor_info_page.dart
+++ example/lib/pages/sensor_info_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:sensing_plugin/sensing_plugin.dart';
 
@@ -11,6 +13,55 @@ class SensorInfoPage extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Sensing Plugin Demo'),
         ),
-        body: Text("$sensorId"),
+        body: Container(
+          padding: const EdgeInsets.only(top: 10),
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Text(
+                sensorId.name,
+                style: const TextStyle(fontSize: 16),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 8.0)),
+              getSensorInfoWidget(sensorId),
+            ],
+          ),
+        ),
       );
 }
+
+FutureBuilder getSensorInfoWidget(SensorId sensorId) => FutureBuilder(
+      future: Future.sync(() {
+        // TODO: call getSensorInfo
+        var sensorInfo = SensorInfo(
+          unit: Unit.bar,
+          accuracy: SensorAccuracy.high,
+          timeIntervalInMilliseconds: 10,
+        );
+        return jsonEncode(sensorInfo.encode());
+      }),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text(
+            "An error occured while loading sensor information",
+            style: TextStyle(color: Colors.red),
+          );
+        }
+
+        if (snapshot.hasData) {
+          var sensorInfo = SensorInfo.decode(jsonDecode(snapshot.data!));
+          return Column(
+            children: [
+              Text("Unit: ${sensorInfo.unit.name}"),
+              Text("Accuracy: ${sensorInfo.accuracy.name}"),
+              Text("Interval: ${sensorInfo.timeIntervalInMilliseconds} ms"),
+            ],
+          );
+        }
+
+        return const Text(
+          "Loading sensor information ...",
+          style: TextStyle(color: Colors.grey),
+        );
+      },
+    );
