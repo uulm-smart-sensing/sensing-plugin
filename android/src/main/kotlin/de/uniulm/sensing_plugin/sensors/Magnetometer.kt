bdc2e0266453713e642e02595b15bcd40ca7ef83
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-03-27 10:34:46 +0200

    feat: Registered Magnetometer in SensingPlugin

--- android/src/main/kotlin/de/uniulm/sensing_plugin/sensors/Magnetometer.kt
+++ android/src/main/kotlin/de/uniulm/sensing_plugin/sensors/Magnetometer.kt
@@ -7,7 +7,7 @@ import de.uniulm.sensing_plugin.streamhandlers.SingleSensorStreamHandler
 
 class Magnetometer(
     sensorManager: SensorManager,
-    timeIntervalInMilliseconds: Long,
+    timeIntervalInMilliseconds: Long
 ) : SingleSensorStreamHandler(
     sensorManager,
     sensorId,
