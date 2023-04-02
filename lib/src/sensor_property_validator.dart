6409b42fc6f661b75b2180297feaeb3c7a9f9c1c
Mukhtar <mukhtar.muse@uni-ulm.de>
2023-04-02 21:14:32 +0200

    feat: created checkUnit,checkTimeInterval and checkUnit

--- lib/src/sensor_property_validator.dart
+++ lib/src/sensor_property_validator.dart
@@ -1,10 +1,37 @@
+import 'generated/api_sensor_manager.dart' show Unit, SensorId;
 
 /// empty class
-class SensorPropertyValidator{
+class SensorPropertyValidator {
+  ///
+  bool checkPrecision(int precision) => precision < 10 || precision > 0;
 
+  ///
+  bool checkTimeInterval(int interval) =>
+      interval < 31536000000 || interval > 0;
 
-
-
-
-
+  ///
+  bool checkUnit(SensorId id, Unit unit) {
+    switch (id) {
+      case SensorId.accelerometer:
+        return unit == Unit.metersPerSecondSquared ||
+            unit == Unit.gravitationalForce;
+      case SensorId.gyroscope:
+        return unit == Unit.radiansPerSecond || unit == Unit.degreesPerSecond;
+      case SensorId.magnetometer:
+        return unit == Unit.microTeslas;
+      case SensorId.orientation:
+        return unit == Unit.radians || unit == Unit.degrees;
+      case SensorId.linearAcceleration:
+        return unit == Unit.metersPerSecondSquared ||
+            unit == Unit.gravitationalForce;
+      case SensorId.barometer:
+        return unit == Unit.hectoPascal ||
+            unit == Unit.kiloPascal ||
+            unit == Unit.bar;
+      case SensorId.thermometer:
+        return unit == Unit.kelvin ||
+            unit == Unit.fahrenheit ||
+            unit == Unit.celsius;
+    }
+  }
 }
