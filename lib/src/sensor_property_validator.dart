b0a420dfd77f5f9aefabc984ee60e16d7c6c24fa
Mukhtar <mukhtar.muse@uni-ulm.de>
2023-04-03 17:47:50 +0200

    test: modified tests

--- lib/src/sensor_property_validator.dart
+++ lib/src/sensor_property_validator.dart
@@ -1,69 +1,68 @@
 import 'generated/api_sensor_manager.dart' show Unit, SensorId;
 
-  /// Checks whether the passed [precision] is valid. It is valid if it is
-  /// between 0 and 10.
-  ///
-  /// Example usage:
-  /// ```dart
-  /// var result = validatePrecision(9);
-  /// ```
-  /// will result in
-  /// ```text
-  /// true
-  /// ```
-  bool validatePrecision(int precision) => precision >= 0 &&  precision <= 10 ;
+/// Checks whether the passed [precision] is valid. It is valid if it is
+/// between 0 and 10.
+///
+/// Example usage:
+/// ```dart
+/// var result = validatePrecision(9);
+/// ```
+/// will result in
+/// ```text
+/// true
+/// ```
+bool validatePrecision(int precision) => precision >= 0 && precision <= 10;
 
-  /// Checks whether the passed [interval] is valid. The unit we use as
-  /// timeInterval is milliseconds. It is valid if the interval is between 10
-  /// and one week, 23 hours, 59 minutes and 59 seconds. The minimum was not 0
-  /// because a sensor can hardly have such high frequencies.
-  ///
-  /// Example usage:
-  /// ```dart
-  /// var result = validateTimeInterval(20);
-  /// ```
-  /// will result in
-  /// ```text
-  /// true
-  /// ```
-  bool validateTimeInterval(int interval) {
-    var almost2Weeks = 601139000;
-    return interval >= 10 && interval <= almost2Weeks;
+/// Checks whether the passed [interval] is valid. The unit we use as
+/// timeInterval is milliseconds. It is valid if the interval is between 10
+/// and one week, 23 hours, 59 minutes and 59 seconds. The minimum was not 0
+/// because a sensor can hardly have such high frequencies.
+///
+/// Example usage:
+/// ```dart
+/// var result = validateTimeInterval(20);
+/// ```
+/// will result in
+/// ```text
+/// true
+/// ```
+bool validateTimeInterval(int interval) {
+  var almost2Weeks = 601139000;
+  return interval >= 10 && interval <= almost2Weeks;
+}
+
+/// Checks whether the passed [Unit] is valid. It is valid if the
+/// corresponding id returns the corresponding unit.
+///
+/// Example usage:
+/// ```dart
+/// var result = validateUnit(Accelerometer,MetersPerSecondSquared);
+/// ```
+/// will result in
+/// ```text
+/// true
+/// ```
+bool validateUnitCompatibility(SensorId id, Unit unit) {
+  switch (id) {
+    case SensorId.accelerometer:
+      return unit == Unit.metersPerSecondSquared ||
+          unit == Unit.gravitationalForce;
+    case SensorId.gyroscope:
+      return unit == Unit.radiansPerSecond || unit == Unit.degreesPerSecond;
+    case SensorId.magnetometer:
+      return unit == Unit.microTeslas;
+    case SensorId.orientation:
+      return unit == Unit.radians || unit == Unit.degrees;
+    case SensorId.linearAcceleration:
+      return unit == Unit.metersPerSecondSquared ||
+          unit == Unit.gravitationalForce;
+    case SensorId.barometer:
+      return unit == Unit.hectoPascal ||
+          unit == Unit.kiloPascal ||
+          unit == Unit.bar;
+    case SensorId.thermometer:
+      return unit == Unit.kelvin ||
+          unit == Unit.fahrenheit ||
+          unit == Unit.celsius;
   }
-
-  /// Checks whether the passed [Unit] is valid. It is valid if the
-  /// corresponding id returns the corresponding unit.
-  ///
-  /// Example usage:
-  /// ```dart
-  /// var result = validateUnit(Accelerometer,MetersPerSecondSquared);
-  /// ```
-  /// will result in
-  /// ```text
-  /// true
-  /// ```
-  bool validateUnit(SensorId id, Unit unit) {
-    switch (id) {
-      case SensorId.accelerometer:
-        return unit == Unit.metersPerSecondSquared ||
-            unit == Unit.gravitationalForce;
-      case SensorId.gyroscope:
-        return unit == Unit.radiansPerSecond || unit == Unit.degreesPerSecond;
-      case SensorId.magnetometer:
-        return unit == Unit.microTeslas;
-      case SensorId.orientation:
-        return unit == Unit.radians || unit == Unit.degrees;
-      case SensorId.linearAcceleration:
-        return unit == Unit.metersPerSecondSquared ||
-            unit == Unit.gravitationalForce;
-      case SensorId.barometer:
-        return unit == Unit.hectoPascal ||
-            unit == Unit.kiloPascal ||
-            unit == Unit.bar;
-      case SensorId.thermometer:
-        return unit == Unit.kelvin ||
-            unit == Unit.fahrenheit ||
-            unit == Unit.celsius;
-    }
-  }
-
+}
--- test/sensor_property_validator_test.dart
+++ test/sensor_property_validator_test.dart
@@ -4,39 +4,75 @@ import 'package:sensing_plugin/src/sensor_property_validator.dart';
 
 void main() {
   group("Validiting precision", () {
-    test('When the numbers correspond to the norm', () {
-      expect(SensorPropertyValidator().checkPrecision(9), isTrue);
+    test('When the precision is valid, then validatePrecision returns true',
+        () {
+      expect(validatePrecision(9), isTrue);
     });
 
-    test('When the numbers do not correspond to the norm', () {
+    test('When the precision is negative, then validatePrecision returns false',
+        () {
       bool result;
-      result = SensorPropertyValidator().checkPrecision(-1);
+      result = validatePrecision(-1);
       expect(result, isFalse);
 
-      result = SensorPropertyValidator().checkPrecision(100);
+      result = validatePrecision(100);
       expect(result, isFalse);
     });
   });
 
-  group("Validiting timeInterval", () {
-    test('When the numbers correspond to the norm', () {
-      expect(SensorPropertyValidator().checkTimeInterval(9), isTrue);
+  group("Validiting timeInterval in milliseconds", () {
+    test('When the interval is valid then validateTimeInterval returns true',
+        () {
+      expect(validateTimeInterval(15), isTrue);
     });
-
-    test('When the numbers do not correspond to the norm', () {
-      bool result;
-      result = SensorPropertyValidator().checkTimeInterval(-1);
-      expect(result, isFalse);
-
-      result = SensorPropertyValidator().checkTimeInterval(31536000001);
-      expect(result, isFalse);
+    test('When the interval is negativ then validateTimeInterval returns false',
+        () {
+      expect(validateTimeInterval(-1), isFalse);
+    });
+    test(
+        "When the interval is too large then validateTimeInterval returns false",
+        () {
+      expect(validateTimeInterval(31536000001), isFalse);
     });
   });
-  test('When the id passes back the appropriate unit', () {
-    expect(
-      SensorPropertyValidator()
-          .checkUnit(SensorId.accelerometer, Unit.metersPerSecondSquared),
-      isTrue,
-    );
+
+  group("Validiting units", () {
+    test(
+        '''When the id passes back the appropriate unit then
+        validateUnitCompatibility return true''',
+        () {
+      expect(
+        validateUnitCompatibility(
+            SensorId.accelerometer, Unit.metersPerSecondSquared),
+        isTrue,
+      );
+    });
+    test(
+        '''When the specified unit does not match the id
+        then validateUnitCompatibility return false''',
+        () {
+      expect(
+        validateUnitCompatibility(SensorId.barometer, Unit.microTeslas),
+        isFalse,
+      );
+    });
+    test(
+        '''When the given id does not correspond to the unit then
+        validateUnitCompatibility return false''',
+        () {
+      expect(
+        validateUnitCompatibility(SensorId.accelerometer, Unit.kelvin),
+        isFalse,
+      );
+    });
+    test(
+        '''When both id and unit are different and have nothing in common then
+        validateUnitCompatibility return false''',
+        () {
+      expect(
+        validateUnitCompatibility(SensorId.gyroscope, Unit.kiloPascal),
+        isFalse,
+      );
+    });
   });
 }
