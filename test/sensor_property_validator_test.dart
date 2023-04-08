689ee9e318aec2b0f9897ffc9a77e0012d975b3b
Mukhtar <mukhtar.muse@uni-ulm.de>
2023-04-08 17:31:18 +0200

    test: added tests

--- test/sensor_property_validator_test.dart
+++ test/sensor_property_validator_test.dart
@@ -11,12 +11,13 @@ void main() {
 
     test('When the precision is negative, then validatePrecision returns false',
         () {
-      bool result;
-      result = validatePrecision(-1);
-      expect(result, isFalse);
+      expect(validatePrecision(-1), isFalse);
+    });
 
-      result = validatePrecision(100);
-      expect(result, isFalse);
+    test(
+        'When the precision is above 10 , then validatePrecision returns false',
+        () {
+      expect(validatePrecision(100), isFalse);
     });
   });
 
@@ -31,15 +32,16 @@ void main() {
       expect(validateIntervalInMilliseconds(-1), isFalse);
     });
     test(
-        "When the interval is too large then validateTimeInterval returns false",
-        () {
-      expect(validateIntervalInMilliseconds(31536000001), isFalse);
-    });
+      "When the interval is too large then validateTimeInterval returns false",
+      () {
+        expect(validateIntervalInMilliseconds(31536000001), isFalse);
+      },
+    );
   });
 
   group("Validiting units", () {
-    test('''When the id passes back accelerometer then
-        validateUnitCompatibility return true''', () {
+    test('''When the id & unit passes back accelerometer and
+      metersPerSecondSquared then validateUnitCompatibility return true''', () {
       expect(
         validateUnitCompatibility(
           SensorId.accelerometer,
@@ -48,7 +50,18 @@ void main() {
         isTrue,
       );
     });
-    test('''When the id passes back gyroscope then
+    test('''When the id & unit passes back accelerometer and gravitationalForce
+      then validateUnitCompatibility return true''', () {
+      expect(
+        validateUnitCompatibility(
+          SensorId.accelerometer,
+          Unit.gravitationalForce,
+        ),
+        isTrue,
+      );
+    });
+
+    test('''When the id & unit passes back gyroscope and radiansPerSecond then
         validateUnitCompatibility return true''', () {
       expect(
         validateUnitCompatibility(
@@ -59,7 +72,18 @@ void main() {
       );
     });
 
-    test('''When the id passes back magnetometer then
+    test('''When the id & unit passes back gyroscope and degreesPerSecond then
+        validateUnitCompatibility return true''', () {
+      expect(
+        validateUnitCompatibility(
+          SensorId.gyroscope,
+          Unit.degreesPerSecond,
+        ),
+        isTrue,
+      );
+    });
+
+    test('''When the id & unit passes back magnetometer and magnometer then
         validateUnitCompatibility return true''', () {
       expect(
         validateUnitCompatibility(
@@ -70,7 +94,7 @@ void main() {
       );
     });
 
-    test('''When the id passes back orientation then
+    test('''When the id & unit passes back orientation and radians then
         validateUnitCompatibility return true''', () {
       expect(
         validateUnitCompatibility(
@@ -81,8 +105,19 @@ void main() {
       );
     });
 
-    test('''When the id passes back linearAcceleration then
+    test('''When the id & unit passes back orientation and degrees then
         validateUnitCompatibility return true''', () {
+      expect(
+        validateUnitCompatibility(
+          SensorId.orientation,
+          Unit.degrees,
+        ),
+        isTrue,
+      );
+    });
+
+    test('''When the id & unit passes back linearAcceleration and
+    metersPerSecondSquared then validateUnitCompatibility return true''', () {
       expect(
         validateUnitCompatibility(
           SensorId.linearAcceleration,
@@ -92,7 +127,18 @@ void main() {
       );
     });
 
-    test('''When the id passes back barometer then
+    test('''When the id & unit passes back linearAcceleration and
+      gravitationalForce then validateUnitCompatibility return true''', () {
+      expect(
+        validateUnitCompatibility(
+          SensorId.linearAcceleration,
+          Unit.gravitationalForce,
+        ),
+        isTrue,
+      );
+    });
+
+    test('''When the id & unit passes back barometer and hectoPascal then
         validateUnitCompatibility return true''', () {
       expect(
         validateUnitCompatibility(
@@ -103,12 +149,55 @@ void main() {
       );
     });
 
-    test('''When the id passes back thermometer then
+    test('''When the id & unit passes back barometer and kiloPascal then
+        validateUnitCompatibility return true''', () {
+      expect(
+        validateUnitCompatibility(
+          SensorId.barometer,
+          Unit.kiloPascal,
+        ),
+        isTrue,
+      );
+    });
+
+    test('''When the id & unit passes back barometer and bar then
+        validateUnitCompatibility return true''', () {
+      expect(
+        validateUnitCompatibility(
+          SensorId.barometer,
+          Unit.bar,
+        ),
+        isTrue,
+      );
+    });
+    test('''When the id & unit passes back thermometer and kelvin then
         validateUnitCompatibility return true''', () {
       expect(
         validateUnitCompatibility(
           SensorId.thermometer,
           Unit.kelvin,
+        ),
+        isTrue,
+      );
+    });
+
+    test('''When the id & unit  passes back thermometer and fahrenheit then
+        validateUnitCompatibility return true''', () {
+      expect(
+        validateUnitCompatibility(
+          SensorId.thermometer,
+          Unit.fahrenheit,
+        ),
+        isTrue,
+      );
+    });
+
+    test('''When the id & unit passes back thermometer and celsius then
+        validateUnitCompatibility return true''', () {
+      expect(
+        validateUnitCompatibility(
+          SensorId.thermometer,
+          Unit.celsius,
         ),
         isTrue,
       );
