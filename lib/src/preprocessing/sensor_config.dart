bb3b123429d5fed43e5a1cfc0c029dd6e3237dec
Felix Schlegel <felix.schlegel@uni-ulm.de>
2023-04-17 13:59:31 +0200

    feat: Added copyWith method for SensorConfig

--- lib/src/preprocessing/sensor_config.dart
+++ lib/src/preprocessing/sensor_config.dart
@@ -20,4 +20,37 @@ class SensorConfig {
     required this.targetPrecision,
     required this.timeInterval,
   });
+
+  /// Creates a copy of this [SensorConfig] with optional changed properties.
+  ///
+  /// If a parameter is not null the value of this [SensorConfig] will be
+  /// overwritten, otherwise (if the value is null or not specified) the value
+  /// will not be overwritten.
+  ///
+  /// Example:
+  /// ```dart
+  /// var config = const SensorConfig(
+  ///   targetUnit: Unit.celsius,
+  ///   targetPrecision: 2,
+  ///   timeInterval: Duration(seconds: 2),
+  /// );
+  ///
+  /// var copy = config.copyWith(targetUnit: Unit.kelvin, timeInterval: null);
+  /// // copy is
+  /// // SensorConfig(
+  /// //   targetUnit: Unit.kelvin,
+  /// //   targetPrecision: 2,
+  /// //   timeInterval: Duration(seconds: 2),
+  /// // )
+  /// ```
+  SensorConfig copyWith({
+    Unit? targetUnit,
+    int? targetPrecision,
+    Duration? timeInterval,
+  }) =>
+      SensorConfig(
+        targetUnit: targetUnit ?? this.targetUnit,
+        targetPrecision: targetPrecision ?? this.targetPrecision,
+        timeInterval: timeInterval ?? this.timeInterval,
+      );
 }
