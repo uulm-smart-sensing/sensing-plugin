b47210612eb742320ac355a1243b5d95055f8f77
Mukhtar <mukhtar.muse@uni-ulm.de>
2023-04-22 01:06:22 +0200

    docs: added comments

--- lib/src/sensor_config_validator.dart
+++ lib/src/sensor_config_validator.dart
@@ -9,7 +9,10 @@ bool validatePrecision(int precision) => precision >= 0 && precision <= 10;
 /// [maxTimeInterval]
 const maxTimeInterval = 691199000;
 
+/// Variable for validating minPrecision
 const configValidatorMinPrecision = 0;
+
+/// Variable for validating maxPrecision
 const configvalidatorMaxPrecision = 10;
 
 /// Checks whether the passed [interval] is valid.It is valid if the interval
@@ -19,14 +22,14 @@ const configvalidatorMaxPrecision = 10;
 bool validateIntervalInMilliseconds(int interval) =>
     interval >= 10 && interval <= maxTimeInterval;
 
-//TODO: will be replaced
-
-///Checks if the matching unit with the corresponding SensorId is the
-///same category/compatible.
+/// TODO: will be replaced
 ///
-///For example a SensorId can be an accelerometer and the corresponding Unit is
-///metersPerSecondSquared or gravitationalForce and if the given Unit does not
-///match then [validateUnitCompatibility] returns false.
+/// Checks if the matching unit with the corresponding SensorId is the
+/// same category/compatible.
+///
+/// For example a SensorId can be an accelerometer and the corresponding Unit is
+/// metersPerSecondSquared or gravitationalForce and if the given Unit does not
+/// match then [validateUnitCompatibility] returns false.
 bool validateUnitCompatibility(SensorId id, Unit unit) {
   switch (id) {
     case SensorId.accelerometer:
