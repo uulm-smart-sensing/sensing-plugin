1ca02c8454bbda5b01c6578aa9031b959b3e422d
Florian Gebhardt <me@fgardt.dev>
2023-04-21 08:58:54 +0200

    feat: export new unit system

--- lib/sensing_plugin.dart
+++ lib/sensing_plugin.dart
@@ -1,12 +1,20 @@
 export 'src/generated/api_sensor_manager.dart'
     show
         SensorId,
-        Unit,
+        SensorUnit,
         SensorTaskResult,
         SensorData,
         SensorInfo,
         SensorAccuracy;
-export 'src/preprocessing/unit_converter.dart' show convertUnit;
 export 'src/sensor_config.dart' show SensorConfig;
 export 'src/sensor_config_validator.dart';
 export 'src/sensor_manager.dart' show SensorManager;
+export 'src/units/unit.dart'
+    show
+        Unit,
+        Acceleration,
+        Angle,
+        AngularVelocity,
+        MagneticFluxDensity,
+        Pressure,
+        Temperature;
