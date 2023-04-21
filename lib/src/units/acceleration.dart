9562ab3ce7596d3a80b047a2788f60ea562956db
Florian Gebhardt <me@fgardt.dev>
2023-04-21 08:15:30 +0200

    feat: add short/long display text

--- lib/src/units/acceleration.dart
+++ lib/src/units/acceleration.dart
@@ -6,27 +6,33 @@ part of unit;
 /// [SensorId.linearAcceleration].
 enum Acceleration implements Unit<Acceleration> {
   /// Acceleration in Gs = Acceleration in m/s^2 / gravity on earth.
-  gravity(9.80665, "G"),
+  gravity(9.80665, "G", "standard gravity"),
 
   /// Acceleration in m/s^2
-  meterPerSecondSquared(1, "m/s²"),
+  meterPerSecondSquared(1, "m/s²", "meter/second²"),
 
   /// Acceleration in in/s^2
-  inchPerSecondSquared(0.0254, "in/s²"),
+  inchPerSecondSquared(0.0254, "in/s²", "inch/second²"),
 
   /// Acceleration in ft/s^2
-  footPerSecondSquared(0.3048, "ft/s²");
+  footPerSecondSquared(0.3048, "ft/s²", "foot/second²");
 
   /// Creates a new enum value for [Acceleration].
-  const Acceleration(this._factor, this._displayText);
+  const Acceleration(
+    this._factor,
+    this._shortDisplayText,
+    this._longDisplayText,
+  );
 
   final num _factor;
-  final String _displayText;
+  final String _shortDisplayText;
+  final String _longDisplayText;
 
   @override
   double convertTo(Acceleration targetUnit, double value) =>
       value / _factor * targetUnit._factor;
 
   @override
-  String toTextDisplay({bool isShort = false}) => _displayText;
+  String toTextDisplay({bool isShort = false}) =>
+      isShort ? _shortDisplayText : _shortDisplayText;
 }
