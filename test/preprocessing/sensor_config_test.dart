e7cf0aa0ed537258725967aaac54656277863fcb
Florian Gebhardt <me@fgardt.dev>
2023-04-21 10:53:54 +0200

    fix: remove redundant import

--- test/preprocessing/sensor_config_test.dart
+++ test/preprocessing/sensor_config_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sensing_plugin/sensing_plugin.dart';
-import 'package:sensing_plugin/src/units/unit.dart';
 
 void main() {
   test(
