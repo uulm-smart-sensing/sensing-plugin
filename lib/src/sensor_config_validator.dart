a964ad055303d13d41f0a2ff94da8d12987374e4
Florian Gebhardt <me@fgardt.dev>
2023-04-22 02:25:25 +0200

    refactor: remove unit validation

--- lib/src/sensor_config_validator.dart
+++ lib/src/sensor_config_validator.dart
@@ -1,5 +1,3 @@
-import 'generated/api_sensor_manager.dart' show Unit, SensorId;
-
 /// Variable for validating minPrecision
 const configValidatorMinPrecision = 0;
 
@@ -23,34 +21,3 @@ const maxTimeInterval = 691199000;
 ///
 bool validateIntervalInMilliseconds(int interval) =>
     interval >= 10 && interval <= maxTimeInterval;
-
-/// TODO: will be replaced
-///
-/// Checks if the matching unit with the corresponding SensorId is the
-/// same category/compatible.
-///
-/// For example a SensorId can be an accelerometer and the corresponding Unit is
-/// metersPerSecondSquared or gravitationalForce and if the given Unit does not
-/// match then [validateUnitCompatibility] returns false.
-bool validateUnitCompatibility(SensorId id, Unit unit) {
-  switch (id) {
-    case SensorId.accelerometer:
-    case SensorId.linearAcceleration:
-      return unit == Unit.metersPerSecondSquared ||
-          unit == Unit.gravitationalForce;
-    case SensorId.gyroscope:
-      return unit == Unit.radiansPerSecond || unit == Unit.degreesPerSecond;
-    case SensorId.magnetometer:
-      return unit == Unit.microTeslas;
-    case SensorId.orientation:
-      return unit == Unit.radians || unit == Unit.degrees;
-    case SensorId.barometer:
-      return unit == Unit.hectoPascal ||
-          unit == Unit.kiloPascal ||
-          unit == Unit.bar;
-    case SensorId.thermometer:
-      return unit == Unit.kelvin ||
-          unit == Unit.fahrenheit ||
-          unit == Unit.celsius;
-  }
-}
